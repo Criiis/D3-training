4833349ba0f8d45e42894113d37e7c4f998784dd
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-22 22:38:01 +0000

    clean app

--- react-D3/src/App.tsx
+++ react-D3/src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useTheme } from "next-themes";
 import Chart from "./components/chart/chart";
+import Header from "./layout/header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { theme, setTheme } = useTheme();
 
   return (
     <>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Chart />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      theme: {theme}
+      <button onClick={() => setTheme("light")}>Light Mode</button>
+      <button onClick={() => setTheme("dark")}>Dark Mode</button>
     </>
   );
 }
