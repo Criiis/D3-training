88a9c6c4f210c4973b7ab6357bc31ba6126c5dcc
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-22 22:37:38 +0000

    add packages and tailwind

---
+++ react-D3/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        simple: "0px 7px 8px -6px rgba(0,0,0,0.37);",
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
