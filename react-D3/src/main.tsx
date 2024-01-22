0b04c0f739c517a8a344689b7758d79e47e7c42d
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-22 22:38:09 +0000

    add theme provider

--- react-D3/src/main.tsx
+++ react-D3/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "next-themes";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <App />
+  </ThemeProvider>
+);
