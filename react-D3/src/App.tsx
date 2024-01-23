6a23d18ceb7e27bf701cb2b50f48b25122975d4a
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-23 20:13:26 +0000

    create cards

--- react-D3/src/App.tsx
+++ react-D3/src/App.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from "next-themes";
 import Chart from "./components/chart/chart";
 import Header from "./layout/header";
+import TotalCard from "./components/total-card";
+import CompaniesCard from "./components/companies-card";
 
 function App() {
   const { theme, setTheme } = useTheme();
@@ -8,11 +10,20 @@ function App() {
   return (
     <>
       <Header />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Chart />
-      theme: {theme}
-      <button onClick={() => setTheme("light")}>Light Mode</button>
-      <button onClick={() => setTheme("dark")}>Dark Mode</button>
+      <main>
+        <TotalCard />
+        <div className="flex flex-wrap gap-4 mt-4">
+          <CompaniesCard />
+          <CompaniesCard />
+          <CompaniesCard />
+          <CompaniesCard />
+        </div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Chart />
+        theme: {theme}
+        <button onClick={() => setTheme("light")}>Light Mode</button>
+        <button onClick={() => setTheme("dark")}>Dark Mode</button>
+      </main>
     </>
   );
 }
---
+++ react-D3/src/components/total-card/total-card.tsx
@@ -0,0 +1,16 @@
+const TotalCard = () => {
+  return (
+    <div className="bg-sky-50 rounded-xl p-3 dark:bg-slate-800 w-[350px] min-h-[200px] flex flex-col justify-between">
+      <p className="m-0 mt-1 font-medium text-s mb-5">Total Holding</p>
+
+      <div>
+        <p className="m-0 font-medium text-3xl">£ 36,248.00</p>
+        <p className="m-0 mt-1 font-medium text-xs">
+          Total Return: <span className="text-base font-bold text-green-400">+362.48 (1.00%)</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TotalCard;
