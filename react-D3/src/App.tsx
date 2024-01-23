d04ef71f979969b6b6151121a42f20e5f086108a
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-23 20:47:57 +0000

    update

--- react-D3/src/App.tsx
+++ react-D3/src/App.tsx
@@ -1,12 +1,8 @@
-import { useTheme } from "next-themes";
-import Chart from "./components/chart/chart";
 import Header from "./layout/header";
 import TotalCard from "./components/total-card";
 import CompaniesCard from "./components/companies-card";
 
 function App() {
-  const { theme, setTheme } = useTheme();
-
   return (
     <>
       <Header />
@@ -18,11 +14,6 @@ function App() {
           <CompaniesCard />
           <CompaniesCard />
         </div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Chart />
-        theme: {theme}
-        <button onClick={() => setTheme("light")}>Light Mode</button>
-        <button onClick={() => setTheme("dark")}>Dark Mode</button>
       </main>
     </>
   );
