c5cf4f4d8c757c7db10ce235271cb2f7ea5de50b
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-23 21:56:21 +0000

    fix css

--- react-D3/src/components/total-card/total-card.tsx
+++ react-D3/src/components/total-card/total-card.tsx
@@ -1,6 +1,6 @@
 const TotalCard = () => {
   return (
-    <div className="bg-[#f0e6ff] rounded-xl p-3 dark:bg-slate-800 w-[350px] min-h-[200px] flex flex-col justify-between">
+    <div className="bg-[#f0e6ff] rounded-xl p-3 dark:bg-slate-800 w-full md:max-w-[350px] min-h-[200px] flex flex-col justify-between">
       <p className="m-0 mt-1 font-medium text-s mb-5">Total Holding</p>
 
       <div>
