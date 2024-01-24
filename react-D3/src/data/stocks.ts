0237b0cbf4e4e275b5ddb89187810ba00c71f797
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-24 22:26:05 +0000

    update data

---
+++ react-D3/src/data/stocks.ts
@@ -0,0 +1,12 @@
+export const stocks = [
+  { symbol: "AAPL", name: "Apple" },
+  { symbol: "GOOGL", name: "Google" },
+  { symbol: "MSFT", name: "Microsoft" },
+  { symbol: "AMZN", name: "Amazon" },
+  { symbol: "META", name: "Meta" },
+  { symbol: "TSLA", name: "Tesla" },
+  { symbol: "UPS", name: "UPS" },
+  { symbol: "AXP", name: "American E." },
+  { symbol: "JNJ", name: "J&J" },
+  { symbol: "WMT", name: "Walmart" },
+];
