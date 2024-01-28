b7038079e2e91156f75641d3874ff23f6c1943d5
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-28 17:45:57 +0000

    fix charts

--- react-D3/src/components/companies-card/chart-preview.tsx
+++ react-D3/src/components/companies-card/chart-preview.tsx
@@ -1,39 +1,48 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import * as d3 from "d3";
 
-const ChartPreview = ({ data }: any) => {
+interface DataPoint {
+  x: number;
+  y: number;
+}
+
+interface ChartPreviewProps {
+  data: DataPoint[];
+}
+
+const ChartPreview = ({ data }: ChartPreviewProps) => {
   const chartRef = useRef<HTMLDivElement>(null);
 
   const color = useMemo(() => {
-    if (data[data.length - 1].y > data[0].y) {
-      return "#4ade80"; // black color
-    }
-    return "#ef4444"; // white color
+    if (data[data.length - 1].y > data[0].y) return "#4ade80";
+    return "#ef4444";
   }, [data]);
 
   useEffect(() => {
-    // Create chart
-    // const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const currentChartRef = chartRef.current;
     const width = 120;
     const height = 60;
 
+    // horizontal line
     const x = d3
       .scaleLinear()
-      .domain(d3.extent(data, (d) => d.x))
+      .domain(d3.extent(data, (d) => d.x) as [number, number])
       .range([0, width]);
 
+    // vertical line
     const y = d3
       .scaleLinear()
-      .domain(d3.extent(data, (d) => d.y))
+      .domain(d3.extent(data, (d) => d.y) as [number, number])
       .range([height, 0]);
 
+    // chart line
     const line = d3
-      .line()
+      .line<DataPoint>()
       .x((d) => x(d.x))
       .y((d) => y(d.y));
 
     const svg = d3
-      .select(chartRef.current)
+      .select(currentChartRef)
       .append("svg")
       .attr("width", width)
       .attr("height", height)
@@ -44,15 +53,15 @@ const ChartPreview = ({ data }: any) => {
       .datum(data)
       .attr("fill", "none")
       .attr("stroke", color)
-      .attr("stroke-width", 1.5)
+      .attr("stroke-width", 2)
       .attr("d", line);
 
     return () => {
-      if (chartRef.current) {
-        chartRef.current.innerHTML = "";
+      if (currentChartRef) {
+        currentChartRef.innerHTML = "";
       }
     };
-  }, [data]);
+  }, [color, data]);
 
   return <div ref={chartRef}></div>;
 };
