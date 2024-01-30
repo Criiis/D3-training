4015ff045b76768689ce27dc86bff5608f134ea3
Criiis <cristiano.luis@wundermanthompson.com>
2024-01-30 14:12:30 +0000

    create holding cards containers

--- react-D3/src/App.tsx
+++ react-D3/src/App.tsx
@@ -4,6 +4,7 @@ import SwiperPreview from "./components/swiper-preview";
 import PreviewCard from "./components/preview-card";
 import { holdings } from "./data/stocks";
 import MainChart from "./components/main-chart";
+import HoldingCard from "./components/holding-card";
 
 // Import Swiper React components
 
@@ -17,31 +18,22 @@ function App() {
         <SwiperPreview className="w-full md:w-homepageTemplate mb-4 md:mb-0" />
         <TotalCard />
       </section>
+
       <section className="flex flex-wrap justify-between flex-col-reverse px-4 pt-4 md:flex-row">
         <div className="w-full md:w-homepageTemplate">
           <MainChart />
         </div>
-
         <div className="md:max-w-[350px] w-full">
-          <div className="bg-gray-100 rounded-xl p-3 dark:bg-zinc-900 w-full flex flex-col justify-between bg-left-bottom bg-no-repeat">
-            <h2 className="mt-0 mb-1">My Holdings</h2>
+          <HoldingCard title="My Portfolio" actionText="View All">
             {holdings.companies.map((data) => {
               return <PreviewCard key={data.symbol} stock={data} />;
             })}
-            <button className="bg-zinc-300 dark:bg-zinc-800  border-0 text-base rounded-lg py-2 px-4 cursor-pointer font-bold">
-              View all
-            </button>
-          </div>
-
-          <div className="bg-gray-100 rounded-xl p-3 dark:bg-zinc-900 w-full flex flex-col justify-between bg-left-bottom bg-no-repeat mt-4">
-            <h2 className="mt-0 mb-1">My Cryptocurrency</h2>
+          </HoldingCard>
+          <HoldingCard title="My Crypto" actionText="Withdraw" className="mt-4">
             {holdings.crypto.map((data) => {
               return <PreviewCard key={data.symbol} stock={data} />;
             })}
-            <button className="bg-zinc-300 dark:bg-zinc-800  border-0 text-base rounded-lg py-2 px-4 cursor-pointer font-bold">
-              Withdraw
-            </button>
-          </div>
+          </HoldingCard>
         </div>
       </section>
     </>
---
+++ react-D3/src/components/holding-card/holding-card.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import Button from "../button";
+import clsx from "clsx";
+
+interface HoldingCardProps {
+  children: ReactNode;
+  title: string;
+  actionText: string;
+  className?: string;
+}
+
+const HoldingCard = ({ children, title, actionText, className }: HoldingCardProps) => {
+  return (
+    <div
+      className={clsx(
+        "bg-gray-100 rounded-xl p-3 dark:bg-zinc-900 w-full flex flex-col justify-between bg-left-bottom bg-no-repeat",
+        className && className
+      )}
+    >
+      <h2 className="mt-0 mb-1">{title}</h2>
+      {children}
+      <Button fullWidth>{actionText}</Button>
+    </div>
+  );
+};
+
+export default HoldingCard;
